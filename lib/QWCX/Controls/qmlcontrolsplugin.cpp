9c37711d5bf140e858cc0d44903d0ce45ad07acc
Alexander <speqtr@users.noreply.github.com>
2019-11-09 18:45:12 +0300

    Added QWCX.Controls.QrCodeItem control

--- lib/QWCX/Controls/qmlcontrolsplugin.cpp
+++ lib/QWCX/Controls/qmlcontrolsplugin.cpp
@@ -1,11 +1,14 @@
 #include <QtQml/qqml.h>
 #include <QWCX/Controls/qmlcontrolsplugin.h>
+#include <QWCX/Controls/qrcodeitem.h>
 
 QWCX_CONTROLS_BEGIN_NAMESPACE
 
 void QmlControlsPlugin::registerTypes(const char *uri)
 {
     qmlRegisterModule(uri, 1, 0);
+
+    qmlRegisterType<QrCodeItem>("QWCX.Controls", 1, 0, "QrCodeItem");
 }
 
 void QmlControlsPlugin::initializeEngine(QQmlEngine *engine, const char *uri)
