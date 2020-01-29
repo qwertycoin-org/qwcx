d31b772256819248ec68dcf0283ab8ddfee698df
Alexander <speqtr@users.noreply.github.com>
2020-01-29 22:52:41 +0300

    Automatically install bundletool when building for Android platform

--- cmake/qwcx/Dependencies.cmake
+++ cmake/qwcx/Dependencies.cmake
@@ -1,3 +1,15 @@
+if(ANDROID)
+    set(ANDROID_BUNDLETOOL_VERSION "0.12.0")
+    set(ANDROID_BUNDLETOOL_URL_PREFIX "https://github.com/google/bundletool/releases/download")
+    set(ANDROID_BUNDLETOOL_URL "${ANDROID_BUNDLETOOL_URL_PREFIX}/${ANDROID_BUNDLETOOL_VERSION}/bundletool-all-${ANDROID_BUNDLETOOL_VERSION}.jar")
+    set(ANDROID_BUNDLETOOL_JAR "${CMAKE_CURRENT_BINARY_DIR}/bundletool-all-0.12.0.jar")
+    if(NOT EXISTS "${ANDROID_BUNDLETOOL_JAR}")
+        message(STATUS "Downloading bundletool v${ANDROID_BUNDLETOOL_VERSION} to ${ANDROID_BUNDLETOOL_JAR}")
+        file(DOWNLOAD "${ANDROID_BUNDLETOOL_URL}" "${ANDROID_BUNDLETOOL_JAR}" SHOW_PROGRESS)
+        set(ANDROID_BUNDLETOOL_JAR "${ANDROID_BUNDLETOOL_JAR}" CACHE STRING "Android bundletool path" FORCE)
+    endif()
+endif()
+
 # (optional) Download and unpack Qt5
 if(DEFINED QT5_DOWNLOAD_VERSION)
     install_qt5(
