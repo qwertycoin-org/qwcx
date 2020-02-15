da0c32f308768f6fc1f5ce52d202f6f54587ce52
Alexander <speqtr@users.noreply.github.com>
2020-02-15 18:03:26 +0300

    Updated CI script with dependencies for Linux platform

--- scripts/ci/linux.sh
+++ scripts/ci/linux.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 sudo apt update
-sudo apt install -y build-essential gcc-8 g++-8 libfontconfig1 libgl1-mesa-dev mesa-common-dev
-sudo apt install -y libpulse0 libpulse-dev # Qt Multimedia
+sudo apt install -y build-essential gcc-5 g++-5 libfontconfig1 libglu1-mesa-dev mesa-common-dev
+sudo apt install -y libpulse0 libpulse-dev # for Qt Multimedia
+sudo apt install -y libgstreamer-plugins-base1.0-0 libasound2 # for Qt plugins (mediaservice)
+sudo apt install -y libxkbcommon-x11-0 libxkbcommon-x11-dev # for Qt plugins (platforms)
+sudo apt install -y libegl1-mesa libegl1-mesa-dev # for Qt platform plugins (xcbglintegrations)
 
 echo "Installing latest CMake..."
 CMAKE_URL=https://github.com/Kitware/CMake/releases/download/v3.16.2/cmake-3.16.2-Linux-x86_64.sh
