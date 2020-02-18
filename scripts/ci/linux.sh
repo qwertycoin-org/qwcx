5fc0e9b8381bd0a59c8b08ee9d132304a455d970
Alexander <speqtr@users.noreply.github.com>
2020-02-18 17:48:18 +0300

    Fixed pre-installed CI dependencies for Linux platform

--- scripts/ci/linux.sh
+++ scripts/ci/linux.sh
@@ -4,7 +4,7 @@ sudo apt update
 sudo apt install -y build-essential gcc-5 g++-5 libfontconfig1 libglu1-mesa-dev mesa-common-dev
 sudo apt install -y libpulse0 libpulse-dev # for Qt Multimedia
 sudo apt install -y libgstreamer-plugins-base1.0-0 libasound2 # for Qt plugins (mediaservice)
-sudo apt install -y libxkbcommon-x11-0 libxkbcommon-x11-dev # for Qt plugins (platforms)
+sudo apt install -y libxcb-* libxkbcommon-x11-0 libxkbcommon-x11-dev # for Qt plugins (platforms)
 sudo apt install -y libegl1-mesa libegl1-mesa-dev # for Qt platform plugins (xcbglintegrations)
 
 echo "Installing latest CMake..."
